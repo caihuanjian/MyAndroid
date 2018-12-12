f6054b89fec1cc50febf450dae2f9d9b74f17394
h4x0r139 <h4x0r_001@163.com>
2018-12-12 16:02:19 +0800

    修复bug

--- my_senior_app/myapp/myapp_01/AppInstallList/src/main/java/cn/yinxm/app/applist/ui/MainActivity.java
+++ my_senior_app/myapp/myapp_01/AppInstallList/src/main/java/cn/yinxm/app/applist/ui/MainActivity.java
@@ -2,16 +2,18 @@ package cn.yinxm.app.applist.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 
 import cn.yinxm.lib.receiver.ApkInstalledReceiver;
 
 public class MainActivity extends SingleFragmentActivity {
     private Fragment mFragment;
 
-	@Override
+    @Override
     protected Fragment createFragment() {
 //        return new AppListFragment();
-        mFragment =  AppInstallFragment.newInstance();
+        mFragment = AppInstallFragment.newInstance();
         return mFragment;
     }
 
@@ -20,15 +22,15 @@ public class MainActivity extends SingleFragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mApkInstalledReceiver = new ApkInstalledReceiver(){
+        mApkInstalledReceiver = new ApkInstalledReceiver() {
             @Override
             public void onApkInstalled(String packageName) {
-                ((AppInstallFragment)mFragment).loadData();
+                ((AppInstallFragment) mFragment).loadData();
             }
 
             @Override
             public void onApkUninstalled(String packageName) {
-                ((AppInstallFragment)mFragment).loadData();
+                ((AppInstallFragment) mFragment).loadData();
             }
         };
 
@@ -40,5 +42,19 @@ public class MainActivity extends SingleFragmentActivity {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mApkInstalledReceiver);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d("yinxm", "keyCode=" + keyCode + ", event=" + event);
+//        if (keyCode == KeyEvent.KEYCODE_BACK) {
+//            Toast.makeText(MainActivity.this, "返回键无效", Toast.LENGTH_SHORT).show();
+//            return true;//return true;拦截事件传递,从而屏蔽back键。
+//        }
+//        if (KeyEvent.KEYCODE_HOME == keyCode) {
+//            Toast.makeText(getApplicationContext(), "HOME 键已被禁用...", Toast.LENGTH_SHORT).show();
+//            return true;//同理
+//        }
+        return super.onKeyDown(keyCode, event);
     }
 }
